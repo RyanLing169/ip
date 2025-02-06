742264793873e2cd03d8972ae164545a4f42244b
RyanLing169 <ryanling12321@gmail.com>
2025-02-07 03:47:41 +0800

    Add Level 2

--- src/main/java/TheGroad.java
+++ src/main/java/TheGroad.java
@@ -2,26 +2,40 @@ import java.util.Scanner;
 
 public class TheGroad {
     public static void main(String[] args) {
-        String line = "________________________________________________________\n" ;
-        String greeting = "____________________________________________________________\n"+
+        String line = "________________________________________________________" ;
+        String greeting = line + "____\n"+
                 "Hello! I'm THE GROAD \n" +
                 "What can I do for you?\n" +
-                "____________________________________________________________";
+                line + "____";
         String bye = "    Bye. Hope to see you again soon!\n";
         String indent = "    ";
         System.out.println(greeting);
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int j = 0;
+
         while (true)
         {
             String input = scanner.nextLine();
             if (input.equals("bye"))
             {
-                System.out.println(indent + line + bye + indent + line);
+                System.out.println(indent + line +"\n" + bye + indent + line);
                 break;
+            }
+            if (input.equals("list"))
+            {
+                System.out.println(indent + line);
+                for (int i = 0 ; i < j ; i += 1)
+                {
+                    System.out.println(indent + (i + 1) + ". " + list[i] );
+                }
+                System.out.println(indent + line);
             }
             else
             {
-                System.out.println(indent + line + indent + input + "\n" + indent + line);
+                System.out.println(indent + line + "\n" + indent + "added: " + input + "\n" + indent + line);
+                list[j] = input;
+                j += 1;
             }
         }
 
