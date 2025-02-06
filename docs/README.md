8827424b98ff4bee585aa3f4c3e48bfac570722d
RyanLing169 <ryanling12321@gmail.com>
2025-02-07 02:31:34 +0800

    Add level 0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# TheGroad User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/TheGroad.java
@@ -0,0 +1,9 @@
+public class TheGroad {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n" ;
+        String greeting = "Hello! I'm THE GROAD \n" +
+                "What can I do for you?\n";
+        String bye = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + greeting + line + bye + line);
+    }
+}
