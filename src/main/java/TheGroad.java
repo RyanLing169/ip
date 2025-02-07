4bd7b5965a58782bdfa988ba60a25d436f009cd8
RyanLing169 <ryanling12321@gmail.com>
2025-02-07 18:15:01 +0800

    Coding Standards

--- src/main/java/TheGroad.java
+++ src/main/java/TheGroad.java
@@ -19,43 +19,33 @@ public class TheGroad {
         Task[] taskList = new Task[100];
         int taskCount = 0;
 
-        while (true)
-        {
+        while (true) {
             String input = scanner.nextLine();
             String[] inputParts = input.split(" ", 2);
 
-            if (input.equals("bye"))
-            {
+            if (input.equals("bye")) {
                 System.out.println(INDENT + LINE +"\n" + BYE + INDENT + LINE);
                 break;
             }
-            if (input.equals("list"))
-            {
+            if (input.equals("list")) {
                 System.out.println(INDENT + LINE);
-                for (int i = 0 ; i < taskCount ; i += 1)
-                {
+                for (int i = 0 ; i < taskCount ; i += 1) {
                     System.out.print(INDENT + (i + 1) + ". ");
                     System.out.println("[" + taskList[i].getStatusIcon() + "] "
                             + taskList[i].description);
                 }
                 System.out.println(INDENT + LINE);
-            }
-            else if (inputParts[0].equals("mark"))
-            {
+            } else if (inputParts[0].equals("mark")) {
                 int taskNum = Integer.parseInt(inputParts[1]);
                 taskList[taskNum - 1].MarkAsDone();
                 System.out.println(INDENT + LINE + "\n" + MARKED);
                 System.out.println(INDENT + "[X] " + taskList[taskNum - 1].description);
-            }
-            else if (inputParts[0].equals("unmark"))
-            {
+            } else if (inputParts[0].equals("unmark")) {
                 int taskNum = Integer.parseInt(inputParts[1]);
                 taskList[taskNum - 1].UnmarkAsDone();
                 System.out.println(INDENT + LINE + "\n" + UNMARKED);
                 System.out.println(INDENT + "[ ] " + taskList[taskNum - 1].description);
-            }
-            else
-            {
+            } else {
                 System.out.println(INDENT + LINE + "\n" + INDENT + "added: " + input + "\n" + INDENT + LINE);
                 taskList[taskCount] = new Task(input);
                 taskCount += 1;
