d164f488cfefbc88cca4b21b9088c8c99efefd3f
RyanLing169 <ryanling12321@gmail.com>
2025-02-07 17:42:26 +0800

    Add Level 3

--- src/main/java/TheGroad.java
+++ src/main/java/TheGroad.java
@@ -2,40 +2,63 @@ import java.util.Scanner;
 
 public class TheGroad {
     public static void main(String[] args) {
-        String line = "________________________________________________________" ;
-        String greeting = line + "____\n"+
+        String LINE = "______________________________" +
+                "__________________________" ;
+        String GREETING = LINE + "____\n"+
                 "Hello! I'm THE GROAD \n" +
                 "What can I do for you?\n" +
-                line + "____";
-        String bye = "    Bye. Hope to see you again soon!\n";
-        String indent = "    ";
-        System.out.println(greeting);
+                LINE + "____";
+        String BYE = "    Bye. Hope to see you again soon!\n";
+        String INDENT = "    ";
+        String MARKED = INDENT + "Nice! I've marked this task as done:";
+        String UNMARKED = INDENT + "OK, I've marked this task as not done yet:";
+
+        System.out.println(GREETING);
+
         Scanner scanner = new Scanner(System.in);
-        String[] list = new String[100];
-        int j = 0;
+        Task[] taskList = new Task[100];
+        int taskCount = 0;
 
         while (true)
         {
             String input = scanner.nextLine();
+            String[] inputParts = input.split(" ", 2);
+
             if (input.equals("bye"))
             {
-                System.out.println(indent + line +"\n" + bye + indent + line);
+                System.out.println(INDENT + LINE +"\n" + BYE + INDENT + LINE);
                 break;
             }
             if (input.equals("list"))
             {
-                System.out.println(indent + line);
-                for (int i = 0 ; i < j ; i += 1)
+                System.out.println(INDENT + LINE);
+                for (int i = 0 ; i < taskCount ; i += 1)
                 {
-                    System.out.println(indent + (i + 1) + ". " + list[i] );
+                    System.out.print(INDENT + (i + 1) + ". ");
+                    System.out.println("[" + taskList[i].getStatusIcon() + "] "
+                            + taskList[i].description);
                 }
-                System.out.println(indent + line);
+                System.out.println(INDENT + LINE);
+            }
+            else if (inputParts[0].equals("mark"))
+            {
+                int taskNum = Integer.parseInt(inputParts[1]);
+                taskList[taskNum - 1].MarkAsDone();
+                System.out.println(INDENT + LINE + "\n" + MARKED);
+                System.out.println(INDENT + "[X] " + taskList[taskNum - 1].description);
+            }
+            else if (inputParts[0].equals("unmark"))
+            {
+                int taskNum = Integer.parseInt(inputParts[1]);
+                taskList[taskNum - 1].UnmarkAsDone();
+                System.out.println(INDENT + LINE + "\n" + UNMARKED);
+                System.out.println(INDENT + "[ ] " + taskList[taskNum - 1].description);
             }
             else
             {
-                System.out.println(indent + line + "\n" + indent + "added: " + input + "\n" + indent + line);
-                list[j] = input;
-                j += 1;
+                System.out.println(INDENT + LINE + "\n" + INDENT + "added: " + input + "\n" + INDENT + LINE);
+                taskList[taskCount] = new Task(input);
+                taskCount += 1;
             }
         }
 
