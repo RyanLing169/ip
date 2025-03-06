f2559ebcd5c3bb3c2b3bf99f7a36b5b916494838
RyanLing169 <ryanling12321@gmail.com>
2025-03-06 13:25:12 +0800

    Level 9

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -68,6 +68,25 @@ public class TaskList {
         ui.printLine();
     }
 
+    public void findTask(String keyword) {
+        Ui.printLine();
+
+        int count = 1;
+
+        for (int i = 0 ; i < taskCount; i += 1) {
+            if (tasks[i].description.contains(keyword)) {
+                Ui.printMessage(INDENT + count + "." + tasks[i]);
+                count += 1;
+            } else if (count == 1 && i == taskCount - 1){
+                Ui.printMessage(INDENT + "SORRY! No tasks contain the keyword :");
+                Ui.printMessage(INDENT + keyword);
+                Ui.printMessage(INDENT + ":-(");
+            }
+        }
+
+        Ui.printLine();
+    }
+
     public Task[] getTasks() {
         return tasks;
     }
